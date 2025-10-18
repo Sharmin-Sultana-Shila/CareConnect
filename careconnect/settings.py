da505315a06b0559bd187458b60ee2034dc6c411
Sharmin Sultana Shila <sharminsultanashila04@gmail.com>
2025-10-18 20:09:04 +0600

    created User model in users

--- careconnect/settings.py
+++ careconnect/settings.py
@@ -115,9 +115,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATICFILES_DIRS = [BASE_DIR / 'static']
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    """User model for customers/clients"""
+    contactNo = models.CharField(max_length=15)
+    address = models.TextField()
+    profile_image = models.ImageField(upload_to='user_profiles/', blank=True, null=True)
+    is_provider = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
