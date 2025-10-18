b6e76e3bf66574bb05e3cd8e653b74b73170a6f4
Shahin <mdshahinakand420@gmail.com>
2025-10-19 01:30:09 +0600

    created login views and urls

--- users/urls.py
+++ users/urls.py
@@ -18,5 +18,18 @@ Including another URLconf
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('login/', views.user_login, name='user_login'),
+    # path('signup/', views.user_signup, name='user_signup'),
+
+
+
+
+
+
+
+
+
+
+
 
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect,get_object_or_404
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from datetime import datetime
+from providers.models import ServiceProvider,Task
+from updates.models import Notification,EmergencySOS
 from .models import User,Feedback,Booking
 
 
@@ -9,3 +11,12 @@ from .models import User,Feedback,Booking
 
 def home(request):
     return render(request, 'home.html')
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None and not user.is_provider:
+            login(request, user)
+            return redirect('user_dashboard')
+    return render(request, 'users/login.html')
