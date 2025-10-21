e9db08359b2b4bc3e7571746bb655bad6ad792be
Isfat Jahan Saifa <isfatsaifasaifa@gmail.com>
2025-10-21 21:50:56 +0600

    defined url for user signup

--- users/urls.py
+++ users/urls.py
@@ -19,7 +19,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('login/', views.user_login, name='user_login'),
-    # path('signup/', views.user_signup, name='user_signup'),
+    path('signup/', views.user_signup, name='user_signup'),
 
 
 
