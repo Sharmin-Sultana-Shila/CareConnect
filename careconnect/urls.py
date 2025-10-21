4283475eb982e09d8d827da583a7fd59cb98c76b
Sharmin Sultana Shila <sharminsultanashila04@gmail.com>
2025-10-21 21:44:54 +0600

    fixed error

--- careconnect/urls.py
+++ careconnect/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', user_views.home, name='home'),
     path('users/', include('users.urls')),
+    path('providers/', include('providers.urls')),
 
 ]
 
