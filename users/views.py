b51bcdaaf50c2d83ed5438ddc344653961ae48ec
Isfat Jahan Saifa <isfatsaifasaifa@gmail.com>
2025-10-21 21:50:10 +0600

    Implement user signup view
    Add user signup functionality with validation.

--- users/views.py
+++ users/views.py
@@ -20,3 +20,33 @@ def user_login(request):
             login(request, user)
             return redirect('user_dashboard')
     return render(request, 'users/login.html')
+    
+
+def user_signup(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        contactNo = request.POST.get('contactNo')
+        address = request.POST.get('address')
+        profile_image = request.FILES.get('profile_image')
+        if password != password2:
+            return redirect('user_signup')
+        if User.objects.filter(username=username).exists():
+            return redirect('user_signup')
+
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=password,
+            contactNo=contactNo,
+            address=address,
+            is_provider=False
+        )
+        if profile_image:
+            user.profile_image = profile_image
+            user.save()
+        return redirect('user_login')
+
+    return render(request, 'users/signup.html')
