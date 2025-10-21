28312c42f08aa376b824e0b386d44dd51317db95
Afroza Sumaya <afrozasumaya76@gmail.com>
2025-10-21 21:24:02 +0600

    defined provider_login view

--- providers/views.py
+++ providers/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect,get_object_or_404
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+
+from django.utils import timezone
+from users.models import User, Booking
+from .models import ServiceProvider, Task
+from updates.models import Notification, EmergencySOS
+
+
 
 # Create your views here.
+
+def provider_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None and user.is_provider:
+            login(request, user)
+            return redirect('provider_dashboard')
+
+    return render(request, 'providers/login.html')
