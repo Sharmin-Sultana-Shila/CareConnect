160864e858f0ce1578bea5138e4f50791f2c308d
Shahin-100 <mdshahinakand420@gmail.com>
2025-10-22 00:07:19 +0600

    url for edit booking

--- users/urls.py
+++ users/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('bookings/', views.user_bookings, name='user_bookings'),
     path('booking/delete/<int:booking_id>/', views.delete_booking, name='delete_booking'),
     path('booking/edit/<int:booking_id>/', views.edit_booking, name='edit_booking'),
+    path('booking/cancel/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),
 
 
 
